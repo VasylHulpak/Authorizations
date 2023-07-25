5e95b8d982fa8704e876f64e85b177d1152ab3b6
vasylhulpak <vh@interlogic.com.ua>
2023-07-25 15:39:56 +0300

    Added validation

---
+++ BasicAuthenticationWebApi/Models/UserValidate.cs
@@ -0,0 +1,13 @@
+﻿namespace BasicAuthenticationWebApi.Models;
+
+public class UserValidate
+{
+	public static bool Login(string username, string password)
+	{
+		UserBL userBL = new UserBL();
+		var UserLists = userBL.GetUsers();
+		return UserLists.Any(user =>
+			user.UserName.Equals(username, StringComparison.OrdinalIgnoreCase)
+			&& user.Password == password);
+	}
+}
