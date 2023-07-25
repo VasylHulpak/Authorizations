a0b45a27d9da6357b788c191bee3d20169b6a1c2
vasylhulpak <vh@interlogic.com.ua>
2023-07-25 15:39:42 +0300

    Create models

---
+++ BasicAuthenticationWebApi/Models/User.cs
@@ -0,0 +1,8 @@
+﻿namespace BasicAuthenticationWebApi.Models;
+
+public class User
+{
+	public int Id { get; set; }
+	public string UserName { get; set; } = null!;
+	public string Password { get; set; } = null!;
+}
