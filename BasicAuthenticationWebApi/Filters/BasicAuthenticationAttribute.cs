8d49b6a25b56d60ca7d2380795b0eda663931945
vasylhulpak <vh@interlogic.com.ua>
2023-07-25 15:40:49 +0300

    Added non finished filter

---
+++ BasicAuthenticationWebApi/Filters/BasicAuthenticationAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Http.Controllers; 
+using System.Web.Http.Filters;
+
+namespace BasicAuthenticationWebApi.Filters;
+
+public class BasicAuthenticationAttribute : AuthorizationFilterAttribute
+{
+	private const string Realm = "My Realm";
+
+	public override void OnAuthorization(HttpActionContext actionContext)
+	{
+		
+	}
+}
