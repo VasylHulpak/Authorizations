afc1599656611f49259080c9f183a29d7eb12ba9
vasylhulpak <vh@interlogic.com.ua>
2023-07-25 15:40:16 +0300

    Added mocked data

---
+++ BasicAuthenticationWebApi/Models/UserBL.cs
@@ -0,0 +1,23 @@
+﻿namespace BasicAuthenticationWebApi.Models;
+
+public class UserBL
+{
+	public List<User> GetUsers()
+	{
+		return new List<User>()
+		{
+			new ()
+			{
+				Id = 101,
+				UserName = "MaleUser",
+				Password = "123456"
+			},
+			new ()
+			{
+				Id = 101,
+				UserName = "FemaleUser",
+				Password = "abcdef"
+			}
+		};
+	}
+}
