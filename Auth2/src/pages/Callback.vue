a8a07964cb355c388e90e31f6dfffba91689233e
vasylhulpak <hulpakvasyl@gmail.com>
2023-08-30 22:53:42 +0300

    Removed unused component

--- Auth2/src/pages/Callback.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div>Callback, {{window.location.href}}</div>
-</template>
-
-<script setup lang="ts">
-console.log(window.location.href)
-</script>
--- Auth2/src/router/index.ts
+++ Auth2/src/router/index.ts
@@ -1,10 +1,8 @@
 import Home from '../App.vue'
-import CallbackComponent from '../pages/Callback.vue'
 import { createMemoryHistory, createRouter } from 'vue-router'
 
 const routes = [
 	{ path: '/', component: Home },
-	{ path: '/callback', component: CallbackComponent },
 ];
 
 const router = createRouter({
